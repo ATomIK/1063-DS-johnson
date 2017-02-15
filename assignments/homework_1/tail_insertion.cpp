9cb42acbf720426d8fb2761abe08dd98b5efb3e8
ATomIK <tom@thomasj.me>
2017-02-15 11:12:47 -0600

    cleaned up program_1; added program_2

--- assignments/homework_1/tail_insertion.cpp
+++ assignments/homework_1/tail_insertion.cpp
@@ -2,8 +2,7 @@
 * @ProgramName: Homework 1 - Tail Insertion
 * @Author: Thomas Johnson
 * @Description:
-*     This program inserts data into a list from the
-*	  beginning of the list and the end.
+*     This program inserts data to the end of a list.
 * @Course: 1063 Data Structures
 * @Semester: Spring 2017
 * @Date: 08 02 2017
@@ -27,69 +26,37 @@ public:
 		Head = NULL;
 	}
 
-	/**
-	* @FunctionName: FrontSert
-	* @Description:
-	*     Inserts x at the beginning of the list and
-	*	  reassigns head to the first node.
-	* @Params:
-	*    none
-	* @Returns:
-	*    void
-	*/
 	void FrontSert(int x) {
-		Node *newData = new Node;
-		newData->Data = x;
-		newData->Next = NULL;
-
-		
+		Node *Temp = new Node;
+		Temp->Data = x;
+		Temp->Next = NULL;
 
 		if (Head) {
-			newData->Next = Head;
+			Temp->Next = Head;
 		}
 
-		Head = newData;
-		
-		// traverse through our data, if the next
-		// is null, that's our end
-		Node *Temp = Head;
-		while (Temp != NULL) {
-			if (Temp->Next == NULL) {
-				Tail = Temp;
-			}
-			Temp = Temp->Next;
-		}
+		Head = Temp;
 
 	}
 
 	/**
-	* @FunctionName: EndSert
+	* @FunctionName: TailSert
 	* @Description:
-	*     Inserts x at the end of the list and reassigns
-	*	  tail to the last node.
+	*     Inserts x at the end of the list.
 	* @Params:
 	*    int x - integer value
 	* @Returns:
 	*    void
 	*/
-	void EndSert(int x) {
-		Node *newData = new Node;
-		newData->Data = x;
-		newData->Next = NULL;
+	void TailSert(int x) {
+		Node *Temp = Head;
 
-		Tail->Next = newData;
-		Tail = Tail->Next;
+		while (Temp != NULL) {
+			cout << Temp->Data << "->";
+			Temp = Temp->Next;
+		}
 	}
 
-	/**
-	* @FunctionName: PrintList
-	* @Description:
-	*     Traverses through our list and cout's them.
-	* @Params:
-	*    none
-	* @Returns:
-	*    void
-	*/
 	void PrintList() {
 		if (!Head) {
 			cout << "Empty" << endl;
@@ -101,6 +68,7 @@ public:
 				cout << Temp->Data << "->";
 				Temp = Temp->Next;
 			}
+			cout << Temp2->Data << endl;
 		}
 	}
 
@@ -113,10 +81,6 @@ int main() {
 	for (int i = 0; i<10; i++) {
 		L.FrontSert(rand() % 100);
 	}
-
-	L.EndSert(50);
-	L.EndSert(70);
-
 	L.PrintList();
 
 
---
+++ assignments/program_2/main.cpp
@@ -0,0 +1,164 @@
+/**
+* @ProgramName: Program-1
+* @Author: Thomas Johnson
+* @Description:
+*     This program reads in images stored as rgb values in a space delimited file format.
+* @Course: 1063 Data Structures
+* @Semester: Spring 2017
+* @Date: 02 06 2017
+*/
+
+#include<iostream>
+#include<fstream>
+#include<math.h>
+
+using namespace std;
+
+/**
+Structure to hold an rgb value
+*/
+struct rgb {
+	int r;
+	int g;
+	int b;
+};
+
+class ImageManip {
+private:
+	rgb** imgArray;
+	int width;
+	int height;
+	ifstream ifile;
+	ofstream ofile;
+	string ifile_name;
+	string ofile_name;
+
+	/**
+	* @FunctionName: readFile
+	* @Description:
+	*     Reads through file assigning rgb values to imgArray after
+	*	  declaring height/width.
+	* @Params:
+	*    none
+	* @Returns:
+	*    void
+	*/
+	void readFile() {
+		ifile.open(ifile_name);
+		ifile >> width >> height;
+
+		imgArray = new rgb*[height];
+
+		for (int i = 0; i<height; i++) {
+			imgArray[i] = new rgb[width];
+		}
+
+		for (int i = 0; i<height; i++) {
+			for (int j = 0; j<width; j++) {
+				ifile >> imgArray[i][j].r >> imgArray[i][j].g >> imgArray[i][j].b;
+			}
+		}
+	}
+
+	/**
+	* @FunctionName: readFile
+	* @Description:
+	*     Writes to out file.
+	* @Params:
+	*    none
+	* @Returns:
+	*    void
+	*/
+	void writeFile() {
+		ofile.open(ofile_name);
+		ofile << width << " " << height << endl;
+		for (int i = 0; i<height; i++) {
+			for (int j = 0; j<width; j++) {
+				ofile << imgArray[i][j].r << " " << imgArray[i][j].g << " " << imgArray[i][j].b << " ";
+			}
+			ofile << endl;
+		}
+	}
+
+public:
+	// constructor isn't required. class is already initialized.
+
+	/**
+	* @FunctionName: flipVert
+	* @Description:
+	*     Loops through rows of RGB values and reverses their row position.
+	* @Params:
+	*    string ifile_n - input file name
+	*    string ofile_n - output file name
+	* @Returns:
+	*    void
+	*/
+	void flipVert(string ifile_n, string ofile_n) {
+		ifile_name = ifile_n;
+		ofile_name = ofile_n;
+		readFile();
+		for (int c = 0; c<width; c++) {
+			imgArray[c] = imgArray[height - 1 - c];
+		}
+	}
+
+	/**
+	* @FunctionName: flipHorz
+	* @Description:
+	*     Loops through columns of RGB values and reverses their column position.
+	* @Params:
+	*    string ifile_n - input file name
+	*    string ofile_n - output file name
+	* @Returns:
+	*    void
+	*/
+	void flipHorz(string ifile_n, string ofile_n) {
+		ifile_name = ifile_n;
+		ofile_name = ofile_n;
+		readFile();
+		for (int r = 0; r<height; r++) {
+			for (int c = 0; c<width; c++) {
+				imgArray[r][c] = imgArray[r][width - 1 - c];
+			}
+		}
+	}
+
+	/**
+	* @FunctionName: grayScale
+	* @Description:
+	*     Loops through a 2D array and turns every RGB value into its grayscale equivalent.
+	* @Params:
+	*    string ifile_n - input file name
+	*    string ofile_n - output file name
+	* @Returns:
+	*    void
+	*/
+	void grayScale(string ifile_n, string ofile_n) {
+		ifile_name = ifile_n;
+		ofile_name = ofile_n;
+		readFile();
+		int avg;
+		for (int r = 0; r < height; r++) {
+			for (int c = 0; c < width; c++) {
+				avg = (imgArray[r][c].r + imgArray[r][c].g + imgArray[r][c].b) / 3;
+				imgArray[r][c].r = avg;
+				imgArray[r][c].g = avg;
+				imgArray[r][c].b = avg;
+			}
+		}
+	}
+
+	// destructor; required to write out results at the end of program
+	~ImageManip() {
+		writeFile();
+	}
+};
+
+int main() {
+
+	ImageManip image;
+	image.grayScale("octocat.txt", "response.txt");
+
+	return 0;
+
+}
